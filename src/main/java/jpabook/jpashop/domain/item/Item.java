8e72768bcca69f018e942d592b664516ef18a089
khs <ewoking777@gmail.com>
2024-01-28 21:47:11 +0900

    섹션 7 : 웹 계층 개발 - 상품 목록

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -21,7 +21,7 @@ public abstract class Item {
 
     private String name;
     private int price;
-    private int stockQunatity;
+    private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
@@ -32,17 +32,17 @@ public abstract class Item {
      * stock 증가
      */
     public void addStock(int quantity) {
-        this.stockQunatity += quantity;
+        this.stockQuantity += quantity;
     }
 
     /**
      * stock 감소
      */
     public void removeStock(int quantity) {
-        int restStock = this.stockQunatity - quantity;
+        int restStock = this.stockQuantity - quantity;
         if (restStock < 0) {
             throw new NotEnoughStockException("need more stock");
         }
-        this.stockQunatity = restStock;
+        this.stockQuantity = restStock;
     }
 }
--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -48,7 +48,7 @@ class OrderServiceTest {
         assertEquals(OrderStatus.ORDER, getOrder.getStatus(),"상품 주문시 상태는 ORDER");
         assertEquals(1, getOrder.getOrderItems().size(), "주문한 상품 종류 수가 정확해야 한다.");
         assertEquals(10000 * orderCount, getOrder.getTotalPrice(), "주문 가격은 가격 * 수량이다.");
-        assertEquals(8, book.getStockQunatity(), "주문 수량만큼 재고가 줄어야 한다.");
+        assertEquals(8, book.getStockQuantity(), "주문 수량만큼 재고가 줄어야 한다.");
     }
 
 
@@ -84,14 +84,14 @@ class OrderServiceTest {
         //then
         Order getOrder = orderRepository.findOne(orderId);
         assertEquals(OrderStatus.CANCEL, getOrder.getStatus(), "주문 취소 시 상태는 CANCEL이다.");
-        assertEquals(10, item.getStockQunatity(), "주문이 취소된 상품은 그만큼 재고가 증가해야 한다.");
+        assertEquals(10, item.getStockQuantity(), "주문이 취소된 상품은 그만큼 재고가 증가해야 한다.");
     }
 
     private Book createBook(String name, int price, int stockQunatity) {
         Book book = new Book();
         book.setName(name);
         book.setPrice(price);
-        book.setStockQunatity(stockQunatity);
+        book.setStockQuantity(stockQunatity);
         em.persist(book);
         return book;
     }
